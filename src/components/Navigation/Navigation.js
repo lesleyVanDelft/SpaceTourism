95dca598ed0b44bdc1151d97098c891363817b11
Lesley van Delft <lesleyvandelft92@gmail.com>
2021-10-27 15:16:59 +0200

    mobile nav functionality added

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,14 +1,25 @@
 import './Navigation.css';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import close from '../../assets/shared/icon-close.svg';
+import hamburger from '../../assets/shared/icon-hamburger.svg';
 
 const Navigation = () => {
-	const handleClick = () => {};
+	const [active, setActive] = useState(false);
+
+	const handleClick = () => {
+		setActive(!active);
+	};
 
 	return (
 		<nav className="Navigation">
-			<ul className="Navigation__list">
-				<img src={close} alt="close icon" className="close" />
+			<img
+				src={`${active ? close : hamburger}`}
+				alt="navigation icon"
+				onClick={handleClick}
+				className="Navigation__icons"
+			/>
+			<ul className={`Navigation__list ${active ? 'active' : null}`}>
 				<li>
 					<Link to="/">
 						<span className="listNumber">00</span>Home
