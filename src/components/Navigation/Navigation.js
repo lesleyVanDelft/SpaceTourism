e55c68db7d71cf634c2fc90e34153ddc3e6ecb66
Lesley van Delft <lesleyvandelft92@gmail.com>
2021-10-27 16:02:17 +0200

    finished mobile navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import close from '../../assets/shared/icon-close.svg';
 import hamburger from '../../assets/shared/icon-hamburger.svg';
+import logo from '../../assets/shared/logo.svg';
 
 const Navigation = () => {
 	const [active, setActive] = useState(false);
@@ -13,31 +14,41 @@ const Navigation = () => {
 
 	return (
 		<nav className="Navigation">
-			<img
-				src={`${active ? close : hamburger}`}
-				alt="navigation icon"
-				onClick={handleClick}
-				className="Navigation__icons"
-			/>
+			<div className="Navigation__icons">
+				<Link to="/">
+					<img src={logo} alt="Navigation__logo" className="logo" />
+				</Link>
+				<img
+					src={`${active ? close : hamburger}`}
+					alt="navigation icon"
+					onClick={handleClick}
+					className="toggle"
+				/>
+			</div>
+
 			<ul className={`Navigation__list ${active ? 'active' : null}`}>
 				<li>
 					<Link to="/">
-						<span className="listNumber">00</span>Home
+						<span className="listNumber">00</span>
+						<span className="listText">Home</span>
 					</Link>
 				</li>
 				<li>
 					<Link to="/">
-						<span className="listNumber">01</span>Destination
+						<span className="listNumber">01</span>
+						<span className="listText">Destination</span>
 					</Link>
 				</li>
 				<li>
 					<Link to="/">
-						<span className="listNumber">02</span>Crew
+						<span className="listNumber">02</span>
+						<span className="listText">Crew</span>
 					</Link>
 				</li>
 				<li>
 					<Link to="/">
-						<span className="listNumber">03</span>Technology
+						<span className="listNumber">03</span>
+						<span className="listText">Technology</span>
 					</Link>
 				</li>
 			</ul>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,22 @@
 import './Home.css';
 import Navigation from '../../components/Navigation/Navigation';
-import hamburger from '../../assets/shared/icon-hamburger.svg';
 
 const Home = () => {
 	return (
 		<div className="Home">
 			<Navigation />
-			<img src={hamburger} alt="Hamburger icon" className="hamburger" />
-			<h1>Hello.</h1>
+
+			<div className="Home__content">
+				<h2>So, you want to travel to</h2>
+				<h1>SPACE</h1>
+				<p>
+					Let’s face it; if you want to go to space, you might as well genuinely
+					go to outer space and not hover kind of on the edge of it. Well sit
+					back, and relax because we’ll give you a truly out of this world
+					experience!
+				</p>
+				<button className="cta">Explore</button>
+			</div>
 		</div>
 	);
 };
