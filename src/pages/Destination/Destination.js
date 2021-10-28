88c5f6baa3fe4ec1c1e56ce19f77dd96e01e895d
Lesley van Delft <lesleyvandelft92@gmail.com>
2021-10-28 14:30:07 +0200

    added destination functionality

--- src/pages/Destination/Destination.js
+++ src/pages/Destination/Destination.js
@@ -6,6 +6,10 @@ import { useState } from 'react';
 const Destination = () => {
 	const [active, setActive] = useState('Moon');
 
+	const getActiveState = e => {
+		setActive(e.target.innerHTML);
+	};
+
 	return (
 		<div className="Destination">
 			<Navigation />
@@ -13,19 +17,43 @@ const Destination = () => {
 				<span className="Heading__number">01</span>
 				<span className="Heading__text">Pick your Destination</span>
 			</div>
+
+			<div className="planetImg">
+				{destinations.map((planet, i) => {
+					return planet.name === active ? (
+						<img src={planet.images.png} alt="" className="" key={i} />
+					) : null;
+				})}
+			</div>
+
 			<div className="Destination__planetNames">
 				<ul>
-					<li>Moon</li>
-					<li>Mars</li>
-					<li>Europa</li>
-					<li>Titan</li>
+					<li
+						onClick={getActiveState}
+						className={`${active === 'Moon' ? 'active' : null}`}>
+						Moon
+					</li>
+					<li
+						onClick={getActiveState}
+						className={`${active === 'Mars' ? 'active' : null}`}>
+						Mars
+					</li>
+					<li
+						onClick={getActiveState}
+						className={`${active === 'Europa' ? 'active' : null}`}>
+						Europa
+					</li>
+					<li
+						onClick={getActiveState}
+						className={`${active === 'Titan' ? 'active' : null}`}>
+						Titan
+					</li>
 				</ul>
 			</div>
-			{destinations.map((planet, i) => {
+			{/* {destinations.map((planet, i) => {
 				{
 				}
-			})}
-			;
+			})} */}
 		</div>
 	);
 };
