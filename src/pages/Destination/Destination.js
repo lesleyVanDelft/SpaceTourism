87fc8fd63855fc06219905ce9a9559700eac37c7
Lesley van Delft <lesleyvandelft92@gmail.com>
2021-11-02 15:45:23 +0100

    fixed destination desktop layout

--- src/pages/Destination/Destination.js
+++ src/pages/Destination/Destination.js
@@ -18,68 +18,72 @@ const Destination = () => {
 				<span className="Heading__text">Pick your Destination</span>
 			</header>
 
-			<div className="planetImg">
-				{destinations.map((planet, i) => {
-					return planet.name === active ? (
-						<img src={planet.images.png} alt="" className="" key={i} />
-					) : null;
-				})}
-			</div>
-
-			<div className="Destination__planetNames">
-				<ul>
-					<li
-						onClick={getActiveState}
-						className={`${active === 'Moon' ? 'active' : null}`}>
-						Moon
-					</li>
-					<li
-						onClick={getActiveState}
-						className={`${active === 'Mars' ? 'active' : null}`}>
-						Mars
-					</li>
-					<li
-						onClick={getActiveState}
-						className={`${active === 'Europa' ? 'active' : null}`}>
-						Europa
-					</li>
-					<li
-						onClick={getActiveState}
-						className={`${active === 'Titan' ? 'active' : null}`}>
-						Titan
-					</li>
-				</ul>
-			</div>
-
-			<h1 className="PlanetTitle">{active}</h1>
-
-			{destinations.map((planet, i) => {
-				return planet.name === active ? (
-					<p className="BodyText" key={i}>
-						{planet.description}
-					</p>
-				) : null;
-			})}
-
-			<div className="TravelInfo">
-				<div className="distance">
-					<h3>Avg. Distance</h3>
+			<main className="Destination__content">
+				<div className="planetImg">
 					{destinations.map((planet, i) => {
 						return planet.name === active ? (
-							<h2 key={i}>{planet.distance}</h2>
+							<img src={planet.images.png} alt="" className="" key={i} />
 						) : null;
 					})}
 				</div>
 
-				<div className="time">
-					<h3>Est. Travel Time</h3>
+				<div className="Destination__textContainer">
+					<div className="Destination__planetNames">
+						<ul>
+							<li
+								onClick={getActiveState}
+								className={`${active === 'Moon' ? 'active' : null}`}>
+								Moon
+							</li>
+							<li
+								onClick={getActiveState}
+								className={`${active === 'Mars' ? 'active' : null}`}>
+								Mars
+							</li>
+							<li
+								onClick={getActiveState}
+								className={`${active === 'Europa' ? 'active' : null}`}>
+								Europa
+							</li>
+							<li
+								onClick={getActiveState}
+								className={`${active === 'Titan' ? 'active' : null}`}>
+								Titan
+							</li>
+						</ul>
+					</div>
+
+					<h1 className="PlanetTitle">{active}</h1>
+
 					{destinations.map((planet, i) => {
 						return planet.name === active ? (
-							<h2 key={i}>{planet.travel}</h2>
+							<p className="BodyText" key={i}>
+								{planet.description}
+							</p>
 						) : null;
 					})}
+
+					<div className="TravelInfo">
+						<div className="distance">
+							<h3>Avg. Distance</h3>
+							{destinations.map((planet, i) => {
+								return planet.name === active ? (
+									<h2 key={i}>{planet.distance}</h2>
+								) : null;
+							})}
+						</div>
+
+						<div className="time">
+							<h3>Est. Travel Time</h3>
+							{destinations.map((planet, i) => {
+								return planet.name === active ? (
+									<h2 key={i}>{planet.travel}</h2>
+								) : null;
+							})}
+						</div>
+					</div>
 				</div>
-			</div>
+			</main>
 		</div>
 	);
 };
