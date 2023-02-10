735cf05391ed860469bbd2b4d5d2cbcfcfba2c17
Lesley van Delft <lesleyvandelft92@gmail.com>
2023-02-10 11:22:34 +0100

    typo fix

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import './Home.css';
 import Navigation from '../../components/Navigation/Navigation';
+import { motion } from 'framer-motion';
 
 const Home = () => {
 	const variants = {
@@ -14,19 +15,21 @@ const Home = () => {
 		},
 	};
 	return (
-		<div className="Home">
+		<motion.div className="Home">
 			<Navigation />
 
-			<motion.div className="Home__content"variants={variants}
-			initial="hidden"
-			animate="show">
+			<div
+				className="Home__content"
+				variants={variants}
+				initial="hidden"
+				animate="show">
 				<div className="text">
 					<h2>So, you want to travel to</h2>
 					<h1>SPACE</h1>
 					<p>
 						Let’s face it; if you want to go to space, you might as well
 						genuinely go to outer space and not hover kind of on the edge of it.
-						Well sit back, and relax because we’ll give you a truly out of this
+						Well sit back, and relax because we'll give you a truly out of this
 						world experience!
 					</p>
 				</div>
